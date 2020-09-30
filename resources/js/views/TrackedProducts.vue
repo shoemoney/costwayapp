25fbf71829c17f4ac40d5ad8c121b40b388264d6
jjcallis <joshuajordancallis@gmail.com>
2020-09-30 21:30:05 +0100

    comment out the logo / notifications

--- resources/js/views/TrackedProducts.vue
+++ resources/js/views/TrackedProducts.vue
@@ -171,33 +171,32 @@
                     </tr>
                   </thead>
                   <tbody class="bg-white divide-y divide-cool-gray-200">
-                    <tr class="bg-white" v-for="tracked in products">
-                      <td class="max-w-0 w-full px-6 py-4 whitespace-no-wrap text-sm leading-5 text-cool-gray-900">
-                        <div class="flex">
-                          <a href="#" class="group inline-flex space-x-2 truncate text-sm leading-5">
-                            <svg class="flex-shrink-0 h-5 w-5 text-cool-gray-400 group-hover:text-cool-gray-500 transition ease-in-out duration-150" viewBox="0 0 20 20" fill="currentColor">
-                              <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
-                            </svg>
-                            <p class="text-cool-gray-500 truncate group-hover:text-cool-gray-900 transition ease-in-out duration-150"
-                            v-text="tracked.product[0].name">
-                             
-                            </p>
-                          </a>
-                        </div>
-                      </td>
-                      <td class="px-6 py-4 text-right whitespace-no-wrap text-sm leading-5 text-cool-gray-500">
-                        <span class="text-cool-gray-900 font-medium" v-text="tracked.product[0].price"></span>
-                        <span v-text="tracked.product[0].currency"></span>
-                      </td>
-                      <td class="hidden px-6 py-4 whitespace-no-wrap text-sm leading-5 text-cool-gray-500 md:block">
-                        <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-green-100 text-green-800 capitalize">
-                          <span v-text="tracked.product[0].metrics[0]['value']"></span>
-                        </span>
-                      </td>
-                      <td class="px-6 py-4 text-right whitespace-no-wrap text-sm leading-5 text-cool-gray-500">
-                        <span v-text="tracked.product[0].updated_at"></span>
-                      </td>
-                    </tr>
+                      <tr class="bg-white" v-for="tracked in products">
+                        <td class="max-w-0 w-full px-6 py-4 whitespace-no-wrap text-sm leading-5 text-cool-gray-900">
+                          <div class="flex">
+                            <a href="#" class="group inline-flex space-x-2 truncate text-sm leading-5">
+                              <svg class="flex-shrink-0 h-5 w-5 text-cool-gray-400 group-hover:text-cool-gray-500 transition ease-in-out duration-150" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+                              </svg>
+                              <p class="text-cool-gray-500 truncate group-hover:text-cool-gray-900 transition ease-in-out duration-150"
+                                v-text="tracked.product[0].name">
+                              </p>
+                            </a>
+                          </div>
+                        </td>
+                        <td class="px-6 py-4 text-right whitespace-no-wrap text-sm leading-5 text-cool-gray-500">
+                          <span class="text-cool-gray-900 font-medium" v-text="tracked.product[0].price"></span>
+                          <span v-text="tracked.product[0].currency"></span>
+                        </td>
+                        <td class="hidden px-6 py-4 whitespace-no-wrap text-sm leading-5 text-cool-gray-500 md:block">
+                          <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-green-100 text-green-800 capitalize">
+                            <span v-text="tracked.product[0].metrics[0]['value']"></span>
+                          </span>
+                        </td>
+                        <td class="px-6 py-4 text-right whitespace-no-wrap text-sm leading-5 text-cool-gray-500">
+                          <span v-text="tracked.product[0].updated_at"></span>
+                        </td>
+                      </tr>
 
                     <!-- More rows... -->
                   </tbody>
--- resources/views/layouts/app/nav.blade.php
+++ resources/views/layouts/app/nav.blade.php
@@ -29,19 +29,14 @@
       </div>
       <div class="flex items-center">
         <div class="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
-          <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition duration-150 ease-in-out" aria-label="Notifications">
+          <!-- <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition duration-150 ease-in-out" aria-label="Notifications">
             <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
             </svg>
-          </button>
+          </button> -->
 
           <!-- Profile dropdown -->
           <div class="ml-3 relative">
-            <div>
-              <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out" id="user-menu" aria-label="User menu" aria-haspopup="true">
-                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-              </button>
-            </div>
             <!--
               Profile dropdown panel, show/hide based on dropdown state.
 
